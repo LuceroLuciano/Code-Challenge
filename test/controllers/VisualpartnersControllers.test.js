ce55c3058d0b4b594a6acc2818c6047675940d26
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-05-30 18:07:41 -0500

    Paso 6: Agregando enpoint y su prueba para optener las lista de visualpartners

---
+++ test/controllers/VisualpartnersControllers.test.js
@@ -0,0 +1,8 @@
+const VisualpartnersController = require('../../lib/controllers/VisualpartnersControllers')
+
+describe("Unit Testing of code challenge", () => {
+    test('1. Get List of Visualpartners', () => {
+        const readFile = VisualpartnersController.getListVisualpartners();
+        expect(readFile[0].name).toBe("Warren");
+    })
+})
