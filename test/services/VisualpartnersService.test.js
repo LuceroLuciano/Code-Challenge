ff6dd514c65c119b70fed8e4cf062efb6212eef1
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-05-31 00:06:24 -0500

    Paso 8: Agregando metodo y prueba para filtrar los emails donde haveCertification = true

--- test/services/VisualpartnersService.test.js
+++ test/services/VisualpartnersService.test.js
@@ -15,6 +15,24 @@ describe("Unit Testing Code Challenge", () => {
             haveCertification: true
         },
         {                            
+            id: "6264d5d85cf81c496446b67f",
+            name: "Lucinda",
+            email: "Sexton@visualpartnership.xyz",
+            credits: 677,
+            enrollments: [
+                "Visual Thinking Avanzado"
+            ],
+            previousCourses: 6,
+            haveCertification: true
+        }];            
+
+        const getEmail = VisualparnertsService.getEmailsWithHaveCertification(students, true)
+        expect(students[0].haveCertification).toBe(true)
+        expect(students[1].haveCertification).toBe(true)
+        expect(students.length).toBe(2)
+    });
+    test("2. Filter all emails of students where haveCertification = true ", () => {
+        const students = [{                            
             id: "6264d5d89f1df827eb84bb23",
             name: "Warren",
             email: "Todd@visualpartnership.xyz",
@@ -25,13 +43,21 @@ describe("Unit Testing Code Challenge", () => {
                 ],
             previousCourses: 1,
             haveCertification: true
-        }
-
-        ];            
-
-        const getEmail = VisualparnertsService.getEmailsWithHaveCertification(students, true)
-        expect(students[0].haveCertification).toBe(true)
-        expect(students[1].haveCertification).toBe(true)
-        expect(students.length).toBe(2)
-    });
+        },
+        {                            
+            id: "6264d5d85cf81c496446b67f",
+            name: "Lucinda",
+            email: "Sexton@visualpartnership.xyz",
+            credits: 677,
+            enrollments: [
+                "Visual Thinking Avanzado"
+            ],
+            previousCourses: 6,
+            haveCertification: true
+        }];
+        const getAllEmailsOfStudents = VisualparnertsService.getEmailsWhereHaveCertificationIsTrue(students, true)
+        expect(students[0].email).toBe("Todd@visualpartnership.xyz")  
+        expect(students[1].email).toBe("Sexton@visualpartnership.xyz")  
+        expect(students.length).toBe(2)    
+    })
 })
